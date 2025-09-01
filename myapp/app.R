f33d443a698b966b517e7d8c6d1af06117794ba4
toddvogel1628 <33038588+toddvogel1628@users.noreply.github.com>
2025-09-01 16:21:57 +0100

    Minor changes for CSC 2025 summer school. Added red points to linear and hyperbolic plots for SV effort.

--- myapp/app.R
+++ myapp/app.R
@@ -8,8 +8,8 @@
 #
 
 library(shiny)
-library(bslib) #for css stuff...
-library(DT) #editable data tables
+library(bslib) # for css stuff...
+library(DT) # editable data tables
 library(ggplot2)
 
 options(shiny.mathjax.url = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js")
@@ -18,8 +18,7 @@ options(shiny.mathjax.url = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.
 ui <- page_fillable(
 
   # Application title
-  titlePanel("CSC 2024 effort discounting demo"),
-  
+  titlePanel("CSC 2025 effort discounting demo"),
   tags$head(tags$script('<script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>')),
 
   # Sidebar with a slider input for number of bins
@@ -27,52 +26,57 @@ ui <- page_fillable(
     border = FALSE,
     fillable = FALSE,
     sidebar = sidebar(
-      tags$label("k param"), #workaround for weird scrolling when updating datatable (https://stackoverflow.com/questions/47048303/r-shiny-how-to-stop-sliderinput-label-click-from-causing-scroll-to-top-of-page)
+      #tags$label("'k' param"), # workaround for weird scrolling when updating datatable (https://stackoverflow.com/questions/47048303/r-shiny-how-to-stop-sliderinput-label-click-from-causing-scroll-to-top-of-page)
       sliderInput("k_param",
-                  label=NULL,
-                  min = 0,
-                  max = 2,
-                  value = 1,
-                  step = 0.01),
+        #label = NULL,
+        label = "'k' parameter",
+        min = 0,
+        max = 2,
+        value = 0.5,
+        step = 0.01
+      ),
       sliderInput("beta",
-                  "beta",
-                  min = 0,
-                  max = 10,
-                  value = 1,
-                  step = 0.1),
+        label = "'beta' parameter",
+        min = 0,
+        max = 5,
+        value = 1,
+        step = 0.1
+      ),
       sliderInput("effort",
-                  "Effort level",
-                  min = 2,
-                  max = 6, 
-                  value = 1,
-                  #step = 0.1),
-                  step = 1),
+        label = "Effort level",
+        min = 2,
+        max = 6,
+        value = 2,
+        # step = 0.1),
+        step = 1
+      ),
       sliderInput("reward",
-                  "Reward level",
-                  min = 2,
-                  max = 6,
-                  value = 1,
-                  #step = 0.1)
-                  step = 1)
+        label = "Reward level",
+        min = 2,
+        max = 6,
+        value = 2,
+        # step = 0.1)
+        step = 1
+      )
     ),
-    
-    #Main page content
+
+    # Main page content
     withMathJax(),
     selectInput("func_form",
-                "Select functional form",
-                choices = list("Linear" = "Linear", "Parabolic" = "Parabolic", "Hyperbolic" = "Hyperbolic"),
-                selected = "Parabolic"
+      "Select functional form",
+      choices = list("Linear" = "Linear", "Parabolic" = "Parabolic", "Hyperbolic" = "Hyperbolic"),
+      selected = "Parabolic"
     ),
     withMathJax(uiOutput("sv_expression")),
     uiOutput("softmax_expression"),
-    
-    #uiOutput("sv_print"),
-    #uiOutput("softmax_print"),
+
+    # uiOutput("sv_print"),
+    # uiOutput("softmax_print"),
     uiOutput("math_output"),
-    
-    plotOutput("effort_plot"),
-    
-    plotOutput("softmax_plot"),
+    fluidRow(
+      column(6, plotOutput("effort_plot")),
+      column(6, plotOutput("softmax_plot"))
+    )
     
   )
 )
@@ -80,29 +84,21 @@ ui <- page_fillable(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
   softmax <- function(SV) {
     exp(SV * input$beta) / (exp(SV * input$beta) + exp(1 * input$beta))
   }
-  
+
   SV_compute <- function() {
     if (input$func_form == "Linear") {
-      SV <- input$reward - (input$k_param*input$effort)
+      SV <- input$reward - (input$k_param * input$effort)
     } else if (input$func_form == "Parabolic") {
-      SV <- input$reward - (input$k_param*(input$effort^2))
+      SV <- input$reward - (input$k_param * (input$effort^2))
     } else if (input$func_form == "Hyperbolic") {
-      SV <- input$reward / (1 + input$k_param*input$effort)
+      SV <- input$reward / (1 + input$k_param * input$effort)
     }
   }
-  
-  
-  testtable = data.frame(
-    ID = "101",
-    reward_level = rep(2:6, times=5),
-    effort_level = rep(2:6, each=5)
-  )
-  
-  
+
+  # Render the SV expression
   output$sv_expression <- renderUI({
     withMathJax(
       if (input$func_form == "Linear") {
@@ -114,60 +110,63 @@ server <- function(input, output) {
       }
     )
   })
-  
+
+  # Render the softmax formula
   output$softmax_expression <- renderUI({
     withMathJax(
-      sprintf("$$P(work) = \\frac{e^{SV*\\beta}}{e^{SV*\\beta} + e^{1*\\beta}}$$")
+      sprintf("$$Pr(work) = \\frac{e^{SV*\\beta}}{e^{SV*\\beta} + e^{1*\\beta}}$$")
     )
   })
-  
+
+  # Render the maths output of the SV and softmax formulas
   output$math_output <- renderUI({
-    
     SV <- SV_compute()
     soft_out <- softmax(SV)
     k_param <- input$k_param
     beta <- input$beta
     effort <- input$effort
-    reward <-input$reward
-    
+    reward <- input$reward
+
     if (input$func_form == "Linear") {
-      sv_text = sprintf("$$%.2f = %i - %.2f*%i$$", SV, reward, k_param, effort)
+      sv_text <- sprintf("$$%.2f = %i - %.2f*%i$$", SV, reward, k_param, effort)
     } else if (input$func_form == "Parabolic") {
-      sv_text = sprintf("$$%.2f = %i - %.2f*%i^2$$", SV, reward, k_param, effort)
+      sv_text <- sprintf("$$%.2f = %i - %.2f*%i^2$$", SV, reward, k_param, effort)
     } else if (input$func_form == "Hyperbolic") {
-      sv_text = sprintf("$$%.2f = \\frac{%i}{1 + %.2f*%i}$$", SV, reward, k_param, effort)
+      sv_text <- sprintf("$$%.2f = \\frac{%i}{1 + %.2f*%i}$$", SV, reward, k_param, effort)
     }
-    
+
+    # print the maths using MathJax
     withMathJax(
       sv_text,
       sprintf("$$%.3f = \\frac{e^{%.2f*%.2f}}{e^{%.2f*%.2f} + e^{1*%.2f}}$$", soft_out, SV, beta, SV, beta, beta)
     )
   })
-  
+
   create_curve <- function() {
-    k_param = input$k_param
-    effort = input$effort
-    
+    k_param <- input$k_param
+    effort <- input$effort
+
     if (input$func_form == "Linear") {
-      curve(-x*k_param, from=2, to=6, ylim=c(-15,-0), xlab = "Effort level", ylab = "SV")
+      curve(-x * k_param, from = 2, to = 6, ylim = c(-15, -0), xlab = "Effort level", ylab = "SV effort")
+      points(x = effort, y = -(effort * k_param), col = "red", bg = "red", pch = 21)
     } else if (input$func_form == "Parabolic") {
-      curve(-((x^2)*k_param), from=2, to=6, ylim=c(-50,0), xlab = "Effort level", ylab = "SV")
-      points(effort, -((effort^2)*k_param), col="red", bg="red", pch=21)
+      curve(-((x^2) * k_param), from = 2, to = 6, ylim = c(-50, 0), xlab = "Effort level", ylab = "SV effort")
+      points(x = effort, y = -((effort^2) * k_param), col = "red", bg = "red", pch = 21)
     } else if (input$func_form == "Hyperbolic") {
-      curve(1/(1+x*k_param), from=2, to=6, ylim=c(0,1), xlab = "Effort level", ylab = "SV")
+      curve(1 / (1 + x * k_param), from = 2, to = 6, ylim = c(0, 1), xlab = "Effort level", ylab = "SV effort")
+      points(x = effort, y = (1 / (1 + k_param * effort)), col = "red", bg = "red", pch = 21)
     }
   }
-  
-  output$effort_plot <- renderPlot({
+
+  output$effort_plot <- renderPlot(
     create_curve()
-  })
-  
+  )
+
   output$softmax_plot <- renderPlot({
-    curve(softmax(x), from=-6, to=6, xlab = "SV work", ylab = "Prob")
+    curve(softmax(x), from = -7, to = 7, xlab = "SV", ylab = "Pr(Work)", ylim = c(0, 1))
     points(SV_compute(), softmax(SV_compute()), col = "red", bg = "red", pch = 21)
   })
 
-  
 }
 
 # Run the application
